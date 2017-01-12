e493400f36bbb573159374c80531be2761a98ead
de0lan10 <deonte.lang@gmail.com>
2017-01-11 21:41:10 -0800

    fxed dirname error

--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -3,10 +3,10 @@ var express = require("express");
 
 module.exports = function(app) {
 	app.get("/survey", function (req, res) {
-		res.sendFile(path.join(_dirname + "/../public/survey.html"))
+		res.sendFile(path.join(__dirname + "/../public/survey.html"))
 	});
 
 	app.use(function (req, res) {
-		res.sendFile(path.join(_dirname + "/../public/home.html"))
+		res.sendFile(path.join(__dirname + "/../public/home.html"))
 	});
 };
